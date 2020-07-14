f07e94430eb07d43bff31a879cbd3ab6b988808e
Krystof <krystofmacek333@gmail.com>
2020-07-14 14:55:02 +0200

    Added Fragments and Navigation

---
+++ app/src/main/java/com/krystofmacek/nyuseu/ui/NewsActivity.kt
@@ -0,0 +1,21 @@
+package com.krystofmacek.nyuseu.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.krystofmacek.nyuseu.R
+import kotlinx.android.synthetic.main.activity_news.*
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+        // setup botton navigation
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+    }
+
+
+}
